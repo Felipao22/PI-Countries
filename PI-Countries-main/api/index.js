319361af7bb675e1ebda0ad5ee286d71f98e91d1
FelipeAviani <felipeaviani@gmail.com>
2023-02-02 18:01:44 -0300

    deploy

--- PI-Countries-main/api/index.js
+++ PI-Countries-main/api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { getCountriesDb } = require('./src/controllers/countryControllers.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     getCountriesDb()
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${port}` ); // eslint-disable-line no-console
   });
 });
