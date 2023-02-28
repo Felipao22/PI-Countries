efc3e2eb9ec8c569fe76a803597f52066046b81e
FelipeAviani <felipeaviani@gmail.com>
2023-02-28 14:45:13 -0300

    deploy vercel

--- PI-Countries-main/client/src/index.js
+++ PI-Countries-main/client/src/index.js
@@ -9,7 +9,8 @@ import dotenv from 'dotenv';
 import axios from 'axios';
 dotenv.config();
 // axios.defaults.baseURL= "http://localhost:3001"
-axios.defaults.baseURL = "https://pi-countries-production-6f95.up.railway.app/";
+// axios.defaults.baseURL = "https://pi-countries-production-6f95.up.railway.app/";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store} >
